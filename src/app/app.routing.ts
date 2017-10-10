7362521b2ef4fab378783569b505779c97f7f818
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-10-10 17:30:53 -0400

    Created the routing file for the links on the website.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,18 +1,38 @@
-/**
- * Created by sesha on 7/26/17.
- */
-
 import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
 import {ModuleWithProviders} from '@angular/core';
+import {LoginComponent} from './components/user/login/login.component';
+import {RegisterComponent} from './components/user/register/register.component';
+import {ProfileComponent} from './components/user/profile/profile.component';
+import {WebsiteListComponent} from './components/website/website-list/website-list.component';
+import {WebsiteEditComponent} from './components/website/website-edit/website-edit.component';
+import {WebsiteNewComponent} from './components/website/website-new/website-new.component';
+import {PageListComponent} from './components/page/page-list/page-list.component';
+import {PageEditComponent} from './components/page/page-edit/page-edit.component';
+import {PageNewComponent} from './components/page/page-new/page-new.component';
+import {WidgetListComponent} from './components/widget/widget-list/widget-list.component';
+import {WidgetEditComponent} from './components/widget/widget-edit/widget-edit.component';
+import {WidgerImageComponent} from './components/widget/widget-edit/widget-image/widget-image.component';
+import {WidgetChooserComponent} from './components/widget/widget-chooser/widget-chooser.component';
+import {WidgetHeaderComponent} from './components/widget/widget-edit/widget-header/widget-header.component';
+import {WidgetYoutubeComponent} from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
+import {HomeComponent} from './components/home/home.component';
 import {TestComponent} from './components/test/test.component';
 
-
-
 const APP_ROUTES: Routes = [
-  {path: '', component : HomeComponent},
-  {path: 'test', component: TestComponent}
+  {path: '', component: HomeComponent},
+  {path: 'test', component: TestComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'user/:userId', component: ProfileComponent},
+  {path: 'user/:userId/website', component: WebsiteListComponent},
+  {path: 'user/:userId/website/new', component: WebsiteNewComponent},
+  {path: 'user/:userId/website/:websiteId', component: WebsiteEditComponent},
+  {path: 'user/:userId/website/:websiteId/page', component: PageListComponent},
+  {path: 'user/:userId/website/:websiteId/page/new', component: PageNewComponent},
+  {path: 'user/:userId/website/:websiteId/page/:pageId', component: PageEditComponent},
+  {path: 'user/:userId/website/:websiteId/page/:pageId/widget', component: WidgetListComponent},
+  {path: 'user/:userId/website/:websiteId/page/:pageId/widget/new', component: WidgetChooserComponent},
+  {path: 'user/:userId/website/:websiteId/page/:pageId/widget/widgetId', component: WidgetEditComponent}
 ];
 
-// Export the routes as module providers
 export const Routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
