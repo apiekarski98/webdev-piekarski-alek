1788f7cf5f483aae7c3f8cfe79b011bd8902b911
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-10-10 12:29:21 -0400

    Created the component files for all of the pages. Filled in the html details.

---
+++ src/app/components/widget/widget-edit/widger-image/widger-image.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WidgerImageComponent } from './widger-image.component';
+
+describe('WidgerImageComponent', () => {
+  let component: WidgerImageComponent;
+  let fixture: ComponentFixture<WidgerImageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WidgerImageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WidgerImageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/widget/widget-edit/widger-image/widger-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-widger-image',
+  templateUrl: './widger-image.component.html',
+  styleUrls: ['./widger-image.component.css']
+})
+export class WidgerImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
