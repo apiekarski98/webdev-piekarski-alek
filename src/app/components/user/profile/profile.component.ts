334db9de216d3b124697cdf9e62c3f85bca84d65
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-10-15 18:12:37 -0400

    Profile page displays the username, first name, and last name.

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../../services/user.service.client';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  userId: String;
+  user = {};
+  username: String;
+  firstName: String;
+  lastName: String;
+
+  constructor(private userService: UserService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((params: any) => {
+      this.userId = params['userId'];
+    });
+
+    this.user = this.userService.findUserById(this.userId);
+    this.username = this.user['username'];
+    this.firstName = this.user['firstName'];
+    this.lastName = this.user['lastName'];
   }
 
 }
