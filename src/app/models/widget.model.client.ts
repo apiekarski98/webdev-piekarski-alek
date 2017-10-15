3d6b3c5a252c037f6c29c94d934c8d6425df5ea7
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-10-15 15:20:47 -0400

    Created the services for the widgets.

---
+++ src/app/models/widget.model.client.ts
@@ -0,0 +1,26 @@
+export class Widget {
+  _id: String;
+  widgetType: String;
+  pageId: String;
+  size: Number;
+  width: String;
+  text: String;
+  url: String;
+  constructor(_id, widgetType, pageId, size, width, text, url) {
+    this._id = _id;
+    this.widgetType = widgetType;
+    this.pageId = pageId;
+    if (widgetType === 'HEADING') {
+      this.size = size;
+      this.text = text;
+
+    }
+    if (widgetType === 'IMAGE' || 'YOUTUBE') {
+      this.width = width;
+      this.url = url;
+    }
+    if (widgetType === 'HTML') {
+      this.text = text;
+    }
+  }
+}
---
+++ src/app/services/widget.service.client.ts
@@ -0,0 +1,63 @@
+import { Widget } from '../models/widget.model.client';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class WidgetService {
+  widgets: Widget[] = [
+    new Widget('123', 'HEADING', '321', 2, 'undefined', 'GIZMODO', 'undefined'),
+    new Widget('234', 'HEADING', '321', 4, 'undefined', 'Lorem ipsum', 'undefined'),
+    new Widget('345', 'IMAGE', '321', 0, '100%', 'undefined', 'http://lorempixel.com/400/200/'),
+    new Widget('456', 'HTML', '321', 0, 'undefined', '<p>Lorem ipsum</p>', 'undefined'),
+    new Widget('567', 'HEADING', '321', 4, 'undefined', 'Lorem ipsum', 'undefined'),
+    new Widget('678', 'YOUTUBE', '321', 0, '100%', 'undefined', 'https://youtu.be/AM2Ivdi9c4E'),
+    new Widget('789', 'HTML', '321', 0, 'undefined', '<p>Lorem ipsum</p>', 'undefined')
+  ];
+
+  api = {
+    'createWidget': this.createWidget,
+    'findWidgetsByPageId': this.findWidgetsByPageId,
+    'findWidgetById': this.findWidgetById,
+    'updateWidget': this.updateWidget,
+    'deleteWidget': this.deleteWidget
+  };
+
+  createWidget(pageId: String, widget: any) {
+    widget._id = pageId;
+    this.widgets.push(widget);
+    return widget;
+  }
+
+  findWidgetsByPageId(pageId: String) {
+    const widgetsByPage = [];
+    for (let i = 0; i < this.widgets.length; i++) {
+      if (this.widgets[i].pageId === pageId) {
+        widgetsByPage.push(this.widgets[i]);
+      }
+    }
+    return widgetsByPage;
+  }
+
+  findWidgetById(widgetId: String) {
+    for (let i = 0; i < this.widgets.length; i++) {
+      if (this.widgets[i]._id === widgetId) {
+        return this.widgets[i];
+      }
+    }
+  }
+
+  updateWidget(widgetId: String, widget: any) {
+    for (let i = 0; i < this.widgets.length; i++) {
+      if (this.widgets[i]._id === widgetId) {
+        this.widgets[i] = widget;
+      }
+    }
+  }
+
+  deleteWidget(widgetId: String) {
+    for (let i = 0; i < this.widgets.length; i++) {
+      if (this.widgets[i]._id === widgetId) {
+        return this.widgets.splice(i, 1);
+      }
+    }
+  }
+}
