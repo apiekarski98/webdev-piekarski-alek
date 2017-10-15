a39bc965c9d8f79388e449eb8e166c0542197caf
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-10-15 19:20:49 -0400

    Page edit pages show the title and description for the page.

--- src/app/components/page/page-edit/page-edit.component.ts
+++ src/app/components/page/page-edit/page-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {PageService} from '../../../services/page.service.client';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-page-edit',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-edit.component.css']
 })
 export class PageEditComponent implements OnInit {
+  pageId: String;
+  page: {};
+  name: String;
+  websiteId: String;
+  description: String;
 
-  constructor() { }
+  constructor(private pageService: PageService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.pageId = params['pageId'];
+        }
+      );
+
+    this.page = this.pageService.findPageById(this.pageId);
+    this.name = this.page['name'];
+    this.description = this.page['description'];
   }
 
 }
