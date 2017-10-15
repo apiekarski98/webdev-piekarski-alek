93cf661877449b712d947e354008cd1a963a2681
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-10-15 18:13:10 -0400

    Created functionality for a user to register.

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {UserService} from '../../../services/user.service.client';
+import {Router} from '@angular/router';
+import {User} from '../../../models/user.model.client';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('f') registerForm: NgForm;
 
-  constructor() { }
+  username: String;
+  password: String;
+  verifyPassword: String;
+  errorFlag: boolean;
+  errorMsg = 'Invalid username or password!';
+
+  constructor(private userService: UserService,
+              private router: Router) {
+
+  }
+
+  register() {
+    this.username = this.registerForm.value.username;
+    this.password = this.registerForm.value.password;
+    this.verifyPassword = this.registerForm.value.verifyPassword;
+    if (this.password === this.verifyPassword) {
+      const user: User = this.userService.createUser(
+        new User('0', this.username, this.password)
+      );
+      this.router.navigate(['/profile', user._id]);
+
+    }
+  }
 
   ngOnInit() {
   }
