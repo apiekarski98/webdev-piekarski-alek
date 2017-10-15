9c44bd8bd62f7d2c6391ba6002dd390ebb60f195
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-10-15 18:43:45 -0400

    Displayed website information on the website edit page.

--- src/app/components/website/website-edit/website-edit.component.ts
+++ src/app/components/website/website-edit/website-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {WebsiteService} from '../../../services/website.service.client';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-website-edit',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./website-edit.component.css']
 })
 export class WebsiteEditComponent implements OnInit {
+  websiteId: String;
+  website: {};
+  name: String;
+  developerId: String;
+  description: String;
 
-  constructor() { }
+  constructor(private websiteService: WebsiteService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.websiteId = params['websiteId'];
+        }
+      );
+
+    this.website = this.websiteService.findWebsiteById(this.websiteId);
+    this.name = this.website['name'];
+    this.description = this.website['description'];
   }
 
 }
