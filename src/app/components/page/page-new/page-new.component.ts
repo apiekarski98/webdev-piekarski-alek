ad333ef4ddf7c738d2ae88240768eaf9d83b5197
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-10-17 00:46:21 -0400

    Added create capabilities for pages.

--- src/app/components/page/page-new/page-new.component.ts
+++ src/app/components/page/page-new/page-new.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PageService} from "../../../services/page.service.client";
+import {ActivatedRoute} from "@angular/router";
+import {Page} from "../../../models/page.model.client";
 
 @Component({
   selector: 'app-page-new',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-new.component.css']
 })
 export class PageNewComponent implements OnInit {
+  websiteId: String;
+  name: String;
+  description: String;
 
-  constructor() { }
+  constructor(private pageService: PageService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.websiteId = params['websiteId'];
+        }
+      );
+  }
+
+  create(name: String, description: String) {
+    this.pageService.createPage(this.websiteId, new Page('', name, this.websiteId, description));
   }
 
 }
