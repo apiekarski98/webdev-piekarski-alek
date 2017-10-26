ba25a11300349dbe13852ad0c86d8e4de2892ace
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-10-26 18:23:45 -0400

    Fixed the widget chooser to create a new element and then direct the user to the appropriate widget edit page.

--- src/app/components/widget/widget-chooser/widget-chooser.component.ts
+++ src/app/components/widget/widget-chooser/widget-chooser.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {WidgetService} from "../../../services/widget.service.client";
+import {Widget} from "../../../models/widget.model.client";
 
 @Component({
   selector: 'app-widget-chooser',
@@ -11,7 +13,10 @@ export class WidgetChooserComponent implements OnInit {
   userId: String;
   websiteId: String;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private widgetService: WidgetService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
+  }
 
   ngOnInit() {
     this.activatedRoute.params
@@ -24,4 +29,8 @@ export class WidgetChooserComponent implements OnInit {
       );
   }
 
+  create(widgetType: String) {
+    var widget = this.widgetService.createWidget(this.pageId, new Widget("", widgetType, this.pageId, 0, "", "", ""));
+    this.router.navigate(['/user', this.userId, 'website', this.websiteId, 'page', this.pageId, 'widget', widget._id]);
+  }
 }
