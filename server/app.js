afc8121ded6f97b5ca7cead3b712490cfbf08eec
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-11-09 16:04:25 -0500

    Required the database in the app file.

--- server/app.js
+++ server/app.js
@@ -11,5 +11,6 @@ module.exports = function (app) {
   require("./../assignment/services/website.service.server.js")(app);
   require("./../assignment/services/page.service.server.js")(app);
   require("./../assignment/services/widget.service.server.js")(app);
+  var db = require("./../assignment/model/model");
 };
 
