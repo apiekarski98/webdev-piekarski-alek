38f5b91b7cc6047141e9bfdb23996dc47b9252c1
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-11-09 19:32:19 -0500

    Built the server side model for the user.

---
+++ assignment/model/user/user.model.server.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+var UserSchema = require("./user.schema.server");
+var UserModel = mongoose.model("UserModel", UserSchema);
+UserModel.createUser = createUser;
+UserModel.findUserById = findUserById;
+UserModel.findUserByCredentials = findUserByCredentials;
+UserModel.findUserByUsername = findUserByUsername;
+UserModel.updateUser = updateUser;
+UserModel.deleteUser = deleteUser;
+
+module.exports = UserModel;
+
+function updateUser(userId, user) {
+  return UserModel.update({_id: userId}, user);
+}
+
+function findUserByUsername(username) {
+  return UserModel.findOne({username: username});
+}
+
+function findUserByCredentials(username, password) {
+  return UserModel.findOne({username: username, password: password});
+}
+
+function createUser(user) {
+  return UserModel.create(user);
+}
+
+function findUserById(userId) {
+  return UserModel.findById(userId);
+}
+
+function deleteUser(userId) {
+  return UserModel.remove({_id: userId});
+}
