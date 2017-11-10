9145eadbd53bb3e2813a57b6adcdee6ca33fcdae
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-11-09 20:42:52 -0500

    Updated the serverside functionality for websites.

--- assignment/services/website.service.server.js
+++ assignment/services/website.service.server.js
@@ -1,6 +1,5 @@
 module.exports = function (app) {
-
-  var WEBSITES = require("./website.mock.server");
+  var websiteModel = require("../model/website/website.model.server");
 
   app.get("/api/user/:userId/website", findWebsitesByUser);
   app.get("/api/user/:userId/website/:websiteId", findWebsiteById);
@@ -9,67 +8,49 @@ module.exports = function (app) {
   app.delete("/api/user/:userId/website/:websiteId", deleteWebsite);
 
   function updateWebsite(req, res) {
-    var userId = req.params['userId'];
     var websiteId = req.params['websiteId'];
     var website = req.body;
-    for (var i = 0; i < WEBSITES.length; i++) {
-      if (WEBSITES[i]._id === websiteId) {
-        WEBSITES[i] = website;
-        var websites = getWebsitesForUserId(userId);
-        res.json(websites);
-        return;
-      }
-    }
+    websiteModel.updateWebsite(websiteId, website)
+      .then(function (status) {
+        res.send(status);
+      });
   }
 
   function findWebsiteById(req, res) {
     var websiteId = req.params['websiteId'];
-    res.json(getWebsiteById(websiteId));
+    websiteModel.findWebsiteById(websiteId)
+      .then(function (website) {
+        res.json(website)
+      });
   }
 
   function deleteWebsite(req, res) {
-    var userId = req.params['userId'];
     var websiteId = req.params['websiteId'];
-    for (var i = 0; i < WEBSITES.length; i++) {
-      if (WEBSITES[i]._id === websiteId) {
-        WEBSITES.splice(i, 1);
-        var websites = getWebsitesForUserId(userId);
-        res.json(websites);
-        return;
-      }
-    }
+    websiteModel.deleteWebsite(websiteId)
+      .then(function (status) {
+        res.send(status);
+      });
   }
 
   function createWebsite(req, res) {
     var userId = req.params['userId'];
     var website = req.body;
     website.developerId = userId;
-    WEBSITES.push(website);
-    var websites = getWebsitesForUserId(userId);
-    res.json(websites);
+    delete website._id;
+    websiteModel.createWebsite(website)
+      .then(function (website) {
+        websiteModel.findWebsitesByUser(userId)
+          .then(function (websites) {
+            res.json(websites);
+          });
+      });
   }
 
   function findWebsitesByUser(req, res) {
     var userId = req.params['userId'];
-    var websites = getWebsitesForUserId(userId);
-    res.json(websites);
-  }
-
-  function getWebsitesForUserId(userId) {
-    var websites = [];
-    for (var i = 0; i < WEBSITES.length; i++) {
-      if (WEBSITES[i].developerId === userId) {
-        websites.push(WEBSITES[i]);
-      }
-    }
-    return websites;
-  }
-
-  function getWebsiteById(websiteId) {
-    for (var i = 0; i < WEBSITES.length; i++) {
-      if (WEBSITES[i]._id === websiteId) {
-        return WEBSITES[i];
-      }
-    }
+    websiteModel.findWebsitesByUser(userId)
+      .then(function (websites) {
+        res.json(websites);
+      });
   }
 }
