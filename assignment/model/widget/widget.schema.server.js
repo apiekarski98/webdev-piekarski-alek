6dd95f5af474bdb5feab1d57960b2c6bcfcda026
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-11-09 22:45:22 -0500

    Created the schema for widgets.

---
+++ assignment/model/widget/widget.schema.server.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var WidgetSchema = mongoose.Schema({
+  _page: {required: true, type: mongoose.Schema.ObjectId, ref: "PageModel"},
+  type: {type: String, enum: ['HEADING', 'IMAGE', 'YOUTUBE', 'HTML', 'INPUT', 'TEXT'], required: true},
+  name: String,
+  text: String,
+  placeholder: String,
+  description: String,
+  url: String,
+  width: String,
+  height: String,
+  rows: Number,
+  size: Number,
+  class: String,
+  icon: String,
+  deletable: Boolean,
+  formatted: Boolean,
+  dateCreated: Date
+}, {collection: 'widget'});
+
+module.exports = WidgetSchema;
