001618ab4c4edc2e48998191c454cadcc6512843
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-11-11 11:58:12 -0500

    Added support for image uploading.

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -1,11 +1,14 @@
 module.exports = function (app) {
   var widgetModel = require("../model/widget/widget.model.server");
+  var multer = require('multer');
+  var upload = multer({ dest: __dirname+'/../../src/assets/uploads' });
 
   app.get("/api/page/:pageId/widget", findWidgetsByPageId);
   app.get("/api/widget/:widgetId", findWidgetById);
   app.post("/api/page/:pageId/widget", createWidget);
   app.put("/api/page/:pageId/widget/:widgetId", updateWidget);
   app.delete("/api/page/:pageId/widget/:widgetId", deleteWidget);
+  app.post ("/api/upload", upload.single('myFile'), uploadImage);
 
   function updateWidget(req, res) {
     var widgetId = req.params['widgetId'];
@@ -53,4 +56,30 @@ module.exports = function (app) {
         res.json(widgets);
       });
   }
+
+  function uploadImage(req, res) {
+
+    var widgetId      = req.body.widgetId;
+    var width         = req.body.width;
+    var myFile        = req.file;
+
+    var userId = req.body.userId;
+    var websiteId = req.body.websiteId;
+    var pageId = req.body.pageId;
+
+    var originalname  = myFile.originalname; // file name on user's computer
+    var filename      = myFile.filename;     // new file name in upload folder
+    var path          = myFile.path;         // full path of uploaded file
+    var destination   = myFile.destination;  // folder where file is saved to
+    var size          = myFile.size;
+    var mimetype      = myFile.mimetype;
+
+    var widget = findWidgetById(widgetId);
+    widget.url = '/uploads/'+filename;
+
+    var callbackUrl   = "/assignment/#/user/" + userId + "/website/" + websiteId + '/widget/' + widgetId;
+
+    res.redirect(callbackUrl);
+  }
+
 }
