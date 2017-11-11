3a891775650d7110aea471418eaf6ed32b45da39
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-11-11 16:25:41 -0500

    Added Quill Editor to the app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { TestComponent } from './components/test/test.component';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
-import { TestService } from './services/test.service.client';
-import { LoginComponent } from './components/user/login/login.component';
-import { ProfileComponent } from './components/user/profile/profile.component';
-import { RegisterComponent } from './components/user/register/register.component';
-import { WebsiteNewComponent } from './components/website/website-new/website-new.component';
-import { WebsiteEditComponent } from './components/website/website-edit/website-edit.component';
-import { WebsiteListComponent } from './components/website/website-list/website-list.component';
-import { PageNewComponent } from './components/page/page-new/page-new.component';
-import { PageEditComponent } from './components/page/page-edit/page-edit.component';
-import { PageListComponent } from './components/page/page-list/page-list.component';
-import { WidgetChooserComponent } from './components/widget/widget-chooser/widget-chooser.component';
-import { WidgetEditComponent } from './components/widget/widget-edit/widget-edit.component';
-import { WidgetListComponent } from './components/widget/widget-list/widget-list.component';
-import { WidgetHeaderComponent } from './components/widget/widget-edit/widget-header/widget-header.component';
-import { WidgetImageComponent } from './components/widget/widget-edit/widget-image/widget-image.component';
-import { WidgetYoutubeComponent } from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
-import { Routing } from './app.routing';
-import { UserService } from './services/user.service.client';
-import { WebsiteService } from './services/website.service.client';
-import { PageService } from './services/page.service.client';
-import { WidgetService } from './services/widget.service.client';
-import { SafePipe } from '../safe-pipe';
-import { FlickrImageSearchComponent } from './components/widget/widget-edit/widget-image/flickr-image-search/flickr-image-search.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {TestComponent} from './components/test/test.component';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {TestService} from './services/test.service.client';
+import {LoginComponent} from './components/user/login/login.component';
+import {ProfileComponent} from './components/user/profile/profile.component';
+import {RegisterComponent} from './components/user/register/register.component';
+import {WebsiteNewComponent} from './components/website/website-new/website-new.component';
+import {WebsiteEditComponent} from './components/website/website-edit/website-edit.component';
+import {WebsiteListComponent} from './components/website/website-list/website-list.component';
+import {PageNewComponent} from './components/page/page-new/page-new.component';
+import {PageEditComponent} from './components/page/page-edit/page-edit.component';
+import {PageListComponent} from './components/page/page-list/page-list.component';
+import {WidgetChooserComponent} from './components/widget/widget-chooser/widget-chooser.component';
+import {WidgetEditComponent} from './components/widget/widget-edit/widget-edit.component';
+import {WidgetListComponent} from './components/widget/widget-list/widget-list.component';
+import {WidgetHeaderComponent} from './components/widget/widget-edit/widget-header/widget-header.component';
+import {WidgetImageComponent} from './components/widget/widget-edit/widget-image/widget-image.component';
+import {WidgetYoutubeComponent} from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
+import {Routing} from './app.routing';
+import {UserService} from './services/user.service.client';
+import {WebsiteService} from './services/website.service.client';
+import {PageService} from './services/page.service.client';
+import {WidgetService} from './services/widget.service.client';
+import {SafePipe} from '../safe-pipe';
+import {FlickrImageSearchComponent} from './components/widget/widget-edit/widget-image/flickr-image-search/flickr-image-search.component';
+import {QuillEditorModule} from 'ngx-quill-editor';
+
 
 @NgModule({
   declarations: [
@@ -57,7 +59,8 @@ import { FlickrImageSearchComponent } from './components/widget/widget-edit/widg
     BrowserModule,
     HttpModule,
     FormsModule,
-    Routing
+    Routing,
+    QuillEditorModule
   ],
   providers: [
     TestService,
@@ -68,4 +71,5 @@ import { FlickrImageSearchComponent } from './components/widget/widget-edit/widg
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
