9487c1e6d858ca6df8a9749bb514b1090dda1d2a
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-11-23 21:33:54 -0500

    Removed a widget array. Stopped creating an id for widgets.

--- src/app/services/widget.service.client.ts
+++ src/app/services/widget.service.client.ts
@@ -1,19 +1,9 @@
-import {Widget} from '../models/widget.model.client';
 import {Injectable} from '@angular/core';
 import {Http} from "@angular/http";
 import {Response} from "@angular/http";
 
 @Injectable()
 export class WidgetService {
-  widgets: Widget[] = [
-    new Widget('123', 'HEADING', '321', 2, 'undefined', 'GIZMODO', 'undefined'),
-    new Widget('234', 'HEADING', '321', 4, 'undefined', 'Lorem ipsum', 'undefined'),
-    new Widget('345', 'IMAGE', '321', 0, '100%', 'undefined', 'http://lorempixel.com/400/200/'),
-    new Widget('456', 'HTML', '321', 0, 'undefined', '<p>Lorem ipsum</p>', 'undefined'),
-    new Widget('567', 'HEADING', '321', 4, 'undefined', 'Lorem ipsum', 'undefined'),
-    new Widget('678', 'YOUTUBE', '321', 0, '100%', 'undefined', 'https://www.youtube.com/embed/AM2Ivdi9c4E'),
-    new Widget('789', 'HTML', '321', 0, 'undefined', '<p>Lorem ipsum</p>', 'undefined')
-  ];
 
   api = {
     'createWidget': this.createWidget,
@@ -26,11 +16,9 @@ export class WidgetService {
   constructor(private http: Http) {
   }
 
-  createWidget(pageId: String, widget: Widget) {
+  createWidget(pageId: String, widget: any) {
     const url = 'http://localhost:3100/api/page/' + pageId + '/widget';
-    widget.pageId = pageId;
-    var randomId = Math.floor(Math.random() * 1000000);
-    widget._id = randomId.toString();
+    widget._page =  pageId;
     return this.http.post(url, widget)
       .map((response: Response) => {
         return response.json();
@@ -53,7 +41,7 @@ export class WidgetService {
       });
   }
 
-  updateWidget(pageId: String, widget: Widget) {
+  updateWidget(pageId: String, widget: any) {
     const url = 'http://localhost:3100/api/page/' + pageId + '/widget/' + widget._id;
     return this.http.put(url, widget)
       .map((response: Response) => {
