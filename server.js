d6314a7bb52d8dff17fe163acb6dd698c3857d60
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-11-27 21:26:12 -0500

    Updated server file to work with the new dependencies.

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
+app.use(cookieParser());
+app.use(session({ secret: process.env.SESSION_SECRET }));
 
 // Point static path to dist -- For building -- REMOVE
 app.use(express.static(path.join(__dirname, 'dist')));
-
-
 
 // CORS
 app.use(function(req, res, next) {
@@ -25,12 +24,8 @@ app.use(function(req, res, next) {
   next();
 });
 
-
-
-
 const port = process.env.PORT || '3100';
 app.set('port', port);
-
 
 // Create HTTP server
 const server = http.createServer(app);
