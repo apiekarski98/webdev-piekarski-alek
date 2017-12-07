cb0042db622ecb273fdd52cb9a4dfeafee94352f
Alek Piekarski <piekarski.a@husky.neu.edu>
2017-12-07 11:18:12 -0500

    Created shared service functionality for the client.

--- src/app/services/user.service.client.ts
+++ src/app/services/user.service.client.ts
@@ -1,11 +1,13 @@
 import {User} from '../models/user.model.client';
 import {Injectable} from '@angular/core';
-import {Http} from "@angular/http";
-import {Response} from "@angular/http";
+import {Http, RequestOptions, Response} from "@angular/http";
 import "rxjs/Rx";
+import {SharedService} from "./shared.service.client";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class UserService {
+  options: RequestOptions = new RequestOptions();
 
   users: User[] = [
     new User('123', 'alice', 'alice', 'Alice', 'Wonder'),
@@ -14,7 +16,9 @@ export class UserService {
     new User('456', 'jannunzi', 'jannunzi', 'Jose', 'Annunzi')
   ];
 
-  constructor(private http: Http) {
+  constructor(private http: Http,
+              private sharedService: SharedService,
+              private router: Router) {
   }
 
   api = {
@@ -25,6 +29,32 @@ export class UserService {
     'updateUser': this.updateUser,
     'deleteUser': this.deleteUser
   };
+
+  register(username, password) {
+    const url = 'http://localhost:3100/api/register';
+    const credentials = {
+      username: username,
+      password: password
+    };
+    this.options.withCredentials = true;
+    return this.http.post(url, credentials, this.options)
+      .map((response: Response) => {
+        return response.json();
+      });
+  }
+
+  login(username, password) {
+    const url = 'http://localhost:3100/api/login';
+    const credentials = {
+      username: username,
+      password: password
+    };
+    this.options.withCredentials = true;
+    return this.http.post(url, credentials, this.options)
+      .map((response: Response) => {
+        return response.json();
+      });
+  }
 
   createUser(user: User) {
     const url = 'http://localhost:3100/api/user';
@@ -76,4 +106,31 @@ export class UserService {
       });
   }
 
+  logout() {
+    const url = 'http://localhost:3100/api/logout';
+    this.options.withCredentials = true;
+    return this.http.post(url, '', this.options)
+      .map((response: Response) => {
+          return response;
+        }
+      );
+  }
+
+  loggedIn() {
+    const url = 'http://localhost:3100/api/loggedIn';
+    this.options.withCredentials = true;
+    return this.http.post(url, '', this.options)
+      .map(
+        (res: Response) => {
+          const user = res.json();
+          if (user !== 0) {
+            this.sharedService.user = user;
+            return true;
+          } else {
+            this.router.navigate(['/login']);
+            return false;
+          }
+        }
+      );
+  }
 }
